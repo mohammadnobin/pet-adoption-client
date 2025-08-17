cbe4c8c204e2daac83fb64bbadbc38188b39448b
MdNobinfd <mdnobin.fdr@gmail.com>
2025-08-18 00:01:06 +0600

    new pets card updated

--- src/components/Home/NewPets.jsx
+++ src/components/Home/NewPets.jsx
@@ -139,46 +139,6 @@ const NewPets = () => {
       </div>
       <div className=" py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {pets.map((pet) => (
-          // <div
-          //   key={pet._id}
-          //   className="relative group custom_gradientl custom_gradientd dark:border-white border-2 border-secondary/15 rounded-2xl shadow-lg overflow-hidden transform transition duration-300 hover:scale-105 hover:shadow-2xl p-4"
-          // >
-          //   {/* Image */}
-          //   <div className="relative h-64 overflow-hidden rounded-t-2xl">
-          //     <img
-          //       src={pet.pteImage}
-          //       alt={pet.petName}
-          //       className="w-full border-b border-secondary/50 h-full object-cover transition-transform duration-500 group-hover:scale-110"
-          //     />
-          //     {/* Category Badge */}
-          //     <span className="absolute bg-secondary top-4 left-4  text-white text-xs font-semibold px-3 py-1 rounded-full shadow-md">
-          //       {pet.petCategory}
-          //     </span>
-          //   </div>
-
-          //   {/* Card Content */}
-          //   <div className="p-6 space-y-2">
-          //     <h3 className="text-2xl font-bold text-secondary dark:text-gray-100">
-          //       {pet.petName}
-          //     </h3>
-          //     <p className="text-sm text-gray-600 dark:text-gray-300">
-          //       Age: {pet.petAge} | Location: {pet.petlocation}
-          //     </p>
-          //     <div className="flex items-center justify-between">
-          //       <p className="text-gray-700 dark:text-gray-200 text-sm line-clamp-3">
-          //         {pet.shortDescription}
-          //       </p>
-          //       <div className="mt-2 text-xs text-gray-400 dark:text-gray-500">
-          //         Added: {new Date(pet.addedAt).toLocaleDateString()}
-          //       </div>
-          //     </div>
-          //   </div>
-
-          //   {/* Optional Hover Overlay */}
-          //   <div className="absolute inset-0 bg-black bg-opacity-10 opacity-0 group-hover:opacity-10 transition-opacity duration-300 rounded-2xl"></div>
-          // </div>
-
-
           <div
   key={pet._id}
   className="relative group rounded-3xl border border-secondary/20 bg-white dark:bg-black text-white shadow-md hover:shadow-2xl transition-all duration-300 overflow-hidden"
